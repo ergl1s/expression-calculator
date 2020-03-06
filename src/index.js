6530830a1b677a6bd4ec1b51b3115dff0d1cfb83
ergl1s <bibikov2000@mail.ru>
2020-03-07 00:31:43 +0300

    feat: add simple calculations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+// Do not use eval!!!
+  return;
+}
+
+const OPERATIONS = {
+  '+' : sum,
+  '-' : dec,
+  '*' : mult,
+  '/' : div,
+}
+
+function sum(first, second) {
+  return first + second;
+}
+function dec(first, second) {
+  return first - second;
+}
+function mult(first, second) {
+  return first * second;
+}
+function div(first, second) {
+  return first / second;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, '');
+  return simpleEval(expr); 
+}
+
+function simpleEval (string) {
+  let first = parseFloat(string);
+  let second = parseFloat(string.slice(String(first).length + 1, string.length));
+  let operation = string[String(first).length];
+  return OPERATIONS[operation](first, second);
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
